ab6135f9e1397805b17a87c875deeb804a921085
Chris Beaumont <beaumont@hawaii.edu>
2013-05-24 07:15:48 -0400

    more packages in .travis.yml

--- test.py
+++ test.py
@@ -3,3 +3,19 @@ from PyQt4 import QtGui
 print 'Hello world'
 app = QtGui.QApplication([''])
 print QtGui.QWidget()
+
+
+import numpy
+print numpy
+
+import scipy
+print scipy
+
+import matplotlib
+print matplotlib
+
+import skimage
+print skimage
+
+import IPython
+print IPython
