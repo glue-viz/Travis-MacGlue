162148633d4f0731dca828b86f463609ce38ff7e
Chris Beaumont <beaumont@hawaii.edu>
2013-05-24 15:25:56 -0400

    qt debugging

---
+++ show_test.py
@@ -0,0 +1,22 @@
+from PyQt4 import QtGui
+
+print 'Hello world'
+app = QtGui.QApplication([''])
+w = QtGui.QWidget()
+w.show()
+
+
+import numpy
+print numpy
+
+import scipy
+print scipy
+
+import matplotlib
+print matplotlib
+
+import skimage
+print skimage
+
+import IPython
+print IPython
