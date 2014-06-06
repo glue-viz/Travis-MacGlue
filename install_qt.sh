51acba8458106b3757a77e404d1a672c6622dbaf
Chris Beaumont <beaumont@hawaii.edu>
2014-06-06 08:58:35 -0400

    bump qt version

--- install_qt.sh
+++ install_qt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
 #binary installation of Qt
-curl -OL http://download.qt-project.org/official_releases/qt/4.8/4.8.6/qt-opensource-mac-4.8.6-1.dmg -o qt.dmg
-hdiutil attach qt.dmg
+curl -OL http://download.qt-project.org/official_releases/qt/4.8/4.8.6/qt-opensource-mac-4.8.6-1.dmg
+hdiutil attach qt-opensource-mac-4.8.6-1.dmg
 sudo installer -pkg /Volumes/Qt\ 4.8.6/Qt.mpkg -target /
