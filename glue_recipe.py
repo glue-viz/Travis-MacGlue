638d200c306eb310758be69d7ff7facbe8fa1d4e
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-02-22 11:46:35 +0000

    We no longer need to patch the icon and ui file paths since that is done in Glue already

--- glue_recipe.py
+++ glue_recipe.py
@@ -1,28 +1,3 @@
-# Ugly hacking to repoint py2app at the right icon, ui files
-
-patch = """
-import os
-from glue.qt import ui, icons, qtutil
-
-def ui_path(ui_name):
-    if not ui_name.endswith('.ui'):
-        ui_name += '.ui'
-
-    result = os.path.dirname(ui.__file__)
-    return os.path.join(result.replace('site-packages.zip', 'glue'),
-                        ui_name)
-
-def icon_path(icon_name):
-    if not icon_name.endswith('.png'):
-        icon_name += '.png'
-    result = os.path.dirname(icons.__file__)
-    return os.path.join(result.replace('site-packages.zip', 'glue'),
-                        icon_name)
-
-qtutil.ui_path = ui_path
-qtutil.icon_path = icon_path
-"""
-
 # We have to manually get the entry points and patch them in glue because py2app
 # doesn't support entry points
 
@@ -33,7 +8,7 @@ PACKAGES = ['glueviz']
 
 # list_plugin_entry_points
 
-patch += """
+patch = """
 from pkg_resources import EntryPoint
 
 def iter_plugin_entry_points():
